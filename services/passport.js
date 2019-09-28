dfe61878ee1f8b8125c1cc143eb67be52130fef6
Md Shayon <mdshayon0@gmail.com>
2019-09-28 20:43:39 +0600

    User is saving to database successfully

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
 
 // The Google authentication strategy authenticates users using a Google account and OAuth 2.0 tokens
 passport.use(
@@ -13,7 +15,19 @@ passport.use(
         // console.log("Access Token: " + accessToken);
         // console.log("Refresh Token: " + refreshToken);
         console.log("profile: " + profile);
-        // console.log("done: " + done);        
+        // console.log("done: " + done);  
+        User.findOne({googleId: profile.id})
+            .then((existingUser)=>{
+                if(existingUser){
+                    console.log('User is already there'); 
+                    done(null, existingUser);
+                }else{
+                    new User({googleId: profile.id}).save()
+                        .then(user=>{
+                            done(null, user);
+                        })
+                }
+            })
     }
     ),
     
